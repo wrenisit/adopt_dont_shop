d6c44ef0449e5361ac9715664c813d7ec30a3c61
Wren Steitle <lauren.steitle@gmail.com>
2019-11-19 16:16:38 -0700

    Set up shelters table in database

--- spec/features/shelters/user_can_see_shelter_index_spec.rb
+++ spec/features/shelters/user_can_see_shelter_index_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "shelter index page", type: :feature do
   it "can see all shelters" do
-    shelter_1 = Shelter.create(name: "Denver Dog Rescue", address: "123 South Street", city: "Denver", state: "CO", zip: 80121)
-    shelter_2 = Shelter.create(name: "Denver Cat Rescue", address: "123 West Street", city: "Denver", state: "CO", zip: 80121)
+    shelter_1 = Shelter.create(name: "Denver Dog Rescue", address: "123 South Street", city: "Denver", state: "CO", zip: "80121")
+    shelter_2 = Shelter.create(name: "Denver Cat Rescue", address: "123 West Street", city: "Denver", state: "CO", zip: "80121")
 
     visit "/shelters"
 
