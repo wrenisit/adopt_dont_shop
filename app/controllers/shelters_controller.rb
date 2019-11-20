91458670b08e47dd7d4982699eac52d8e370bca7
Wren Steitle <lauren.steitle@gmail.com>
2019-11-20 16:23:35 -0700

    Commit with setup for new shelter form

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,3 +1,15 @@
 class SheltersController < ApplicationController
+  def index
+    @shelters = Shelter.all
+  end
 
+  def show
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
 end
--- spec/features/shelters/user_can_see_shelter_index_spec.rb
+++ spec/features/shelters/user_can_see_shelter_index_spec.rb
@@ -8,15 +8,8 @@ RSpec.describe "shelter index page", type: :feature do
     visit "/shelters"
 
     expect(page).to have_content(shelter_1.name)
-    expect(page).to have_content(shelter_1.address)
-    expect(page).to have_content(shelter_1.city)
-    expect(page).to have_content(shelter_1.state)
-    expect(page).to have_content(shelter_1.zip)
 
     expect(page).to have_content(shelter_2.name)
-    expect(page).to have_content(shelter_2.address)
-    expect(page).to have_content(shelter_2.city)
-    expect(page).to have_content(shelter_2.state)
-    expect(page).to have_content(shelter_2.zip)
+
   end
 end
