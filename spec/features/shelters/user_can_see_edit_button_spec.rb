20e67094d36c4b48ae3b811d3b2193c14407548d
Wren Steitle <lauren.steitle@gmail.com>
2019-11-22 17:35:01 -0700

    Complete add edit botton to shelter from index page

---
+++ spec/features/shelters/user_can_see_edit_button_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "user can click button to get to shelter edit page" do
+  it "has button to edit shelter" do
+    shelter_1 = Shelter.create(name: "Denver Dog Rescue", address: "123 South Street", city: "Denver", state: "CO", zip: "80121")
+
+    visit '/shelters'
+    click_on "Update"
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+
+    fill_in :name, with: "Washington Wags"
+    fill_in :address, with: "2222 Bee Street"
+    fill_in :city, with: "Monroe"
+    fill_in :state, with: "WA"
+    fill_in :zip, with: "33321"
+
+    click_on "Update Shelter"
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}")
+
+    expect(page).to have_content("Washington Wags")
+    expect(page).to have_content("2222 Bee Street")
+    expect(page).to have_content("Monroe")
+    expect(page).to have_content("WA")
+    expect(page).to have_content("33321")
+
+    expect(page).to_not have_content("Denver Dog Rescue")
+    expect(page).to_not have_content("123 South Street")
+    expect(page).to_not have_content("Denver")
+    expect(page).to_not have_content("CO")
+    expect(page).to_not have_content("80121")
+  end
+end
+    # When I visit the shelter index page
+    # Next to every shelter, I see a link to edit that shelter's info
+    # When I click the link
+    # I should be taken to that shelters edit page where I can update its information just like in User Story 5
